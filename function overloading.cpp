48bfc5785bad5ab5eb540955f16c2eb2ee754c85
AreebIdrees <141308164+AreebIdrees@users.noreply.github.com>
2023-09-12 15:48:54 +0530

    Add files via upload

---
+++ function overloading.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+double sum(double num1,double num2,int num3){
+    return num1+num2+num3;
+}
+double sum(double num1,int num2,int num3){
+    return num1+num2+num3;
+}
+double sum(int num1,double num2,int num3){
+    return num1+num2+num3;
+}
+int sum(int num1,int num2,int num3){
+    return num1+num2+num3;
+}                
+int main(){
+cout<<sum(8.9,6.9,4)<<endl;
+
+cout<<sum(8.9,4,8)<<endl;
+
+cout<<sum(8,6.9,7)<<endl;
+
+cout<<sum(8,9,5)<<endl;
+
+
+
+
+}
